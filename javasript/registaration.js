5e75c120b87baddde98122d9f8439a858555b6d7
Arun CB <aruncbhaskaran@gmail.com>
2026-02-03 12:04:02 +0530

    the hiring static page is created

---
+++ javasript/registaration.js
@@ -0,0 +1,19 @@
+
+        function showFields() {
+        
+          // Get selected radio value
+          let role = document.querySelector('input[name="role"]:checked').value;
+          
+          // Hide both first
+          document.getElementById("employBox").style.display = "none";
+          document.getElementById("providerBox").style.display = "none";
+        
+          // Show based on selection
+          if (role === "Employ") {
+            document.getElementById("employBox").style.display = "block";
+          }
+        
+          if (role === "provider") {
+            document.getElementById("providerBox").style.display = "block";
+          }
+        }
